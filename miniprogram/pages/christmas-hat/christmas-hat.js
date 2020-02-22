5c0398eb807b537251d4c6157dd59699f017bff8
shenghanqin <shenghanqin@hujiang.com>
2020-02-22 15:39:48 +0800

    update 继续调试页面功能

--- miniprogram/pages/christmas-hat/christmas-hat.js
+++ miniprogram/pages/christmas-hat/christmas-hat.js
@@ -1,5 +1,68 @@
 // pages/christmas-hat/christmas-hat.js
-import utilsCommon from '../../utils/common';
+import utilsCommon from '../../utils/common'
+import promisify from '../../utils/promisify';
+
+const { windowWidth, pixelRatio } = utilsCommon.getSystemInfo()
+const CANVAS_SIZE = 300
+const PageDpr = windowWidth / 375
+
+const DPR_CANVAS_SIZE = CANVAS_SIZE * PageDpr
+const SAVE_IMAGE_WIDTH = DPR_CANVAS_SIZE * pixelRatio
+const DEFAULT_MASK_SIZE = 100 * PageDpr
+const MASK_SIZE = 100
+
+const resetState = () => {
+  return {
+    maskWidth: DEFAULT_MASK_SIZE,
+    currentMaskId: 1,
+    timeNow: Date.now(),
+
+    maskCenterX: DPR_CANVAS_SIZE / 2,
+    maskCenterY: DPR_CANVAS_SIZE / 2,
+    resizeCenterX: DPR_CANVAS_SIZE / 2 + DEFAULT_MASK_SIZE / 2 - 2,
+    resizeCenterY: DPR_CANVAS_SIZE / 2 + DEFAULT_MASK_SIZE / 2 - 2,
+    rotate: 0,
+    reserve: 1
+  }
+}
+
+const setTmpThis = (el, elState) => {
+  const {
+    maskWidth,
+    maskCenterX,
+    maskCenterY,
+    resizeCenterX,
+    resizeCenterY,
+    rotate
+  } = elState
+
+  el.mask_width = maskWidth
+  el.mask_center_x = maskCenterX;
+  el.mask_center_y = maskCenterY;
+  el.resize_center_x = resizeCenterX;
+  el.resize_center_y = resizeCenterY;
+
+  el.rotate = rotate;
+
+  el.touch_target = '';
+  el.touch_shape_index = -1;
+
+}
+
+const materialList = [
+  {
+    name: 'mask',
+    icon: '../../images/icon-category-mask.png',
+    imgList: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    type: 'multi'
+  },
+  {
+    name: 'jiayou',
+    icon: '../../images/icon-category-jiayou.png',
+    imgList: [1, 2, 3, 4, 5, 6],
+    type: 'single'
+  }
+]
 
 Page({
 
@@ -8,7 +71,19 @@ Page({
    */
   data: {
     DPR_CANVAS_SIZE: 300,
-    pixelRatio: utilsCommon.getSystemInfo().pixelRatio
+    pixelRatio: utilsCommon.getSystemInfo().pixelRatio,
+    shapeList: [
+      resetState()
+    ],
+    materialList: [],
+    currentShapeIndex: 0,
+    originSrc: '',
+    cutImageSrc: '',
+    posterSrc: '',
+    isShowPoster: false,
+    currentJiayouId: 1,
+    currentTabIndex: 0,
+    isShowMask: false,
   },
 
   /**
@@ -65,5 +140,22 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  onChooseImage(way){
+
+    // console.log('event :', event);
+    // TODO 兼容写法
+    // let way = event.target.dataset.way || 'album'
+    const chooseImage = promisify(wx.chooseImage)
+    chooseImage({
+      count: 1,
+      sourceType: [way],
+    }).then(res => {
+      this.setData({
+        originSrc: res.tempFilePaths[0]
+      });
+    }).catch(error => {
+      console.log('error :', error);
+    })
   }
 })
