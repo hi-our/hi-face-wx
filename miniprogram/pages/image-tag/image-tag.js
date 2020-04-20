0c6ec37a62295df2efe61027a3643317c417c902
shenghanqin <shenghanqin@163.com>
2020-04-20 20:56:33 +0800

    update 更新图像标签，review代码

--- miniprogram/pages/image-tag/image-tag.js
+++ miniprogram/pages/image-tag/image-tag.js
@@ -5,16 +5,26 @@ Page({
     bigPic: '/images/bigPic.png',
     imgTags: [],
   },
+  //分享
+  onShareAppMessage: function () {
+    const DEFAULT_SHARE_COVER = 'https://n1image.hjfile.cn/res7/2020/02/02/a374bb58c4402a90eeb07b1abbb95916.png'
 
+    return {
+      title: '图像标签',
+      imageUrl: DEFAULT_SHARE_COVER,
+      path: '/pages/image-tag/image-tag'
+    }
+  },
   async mainFunc() {
     try {
-      const imgPaths = await this.chooseImg()
+      const imgPath = await this.chooseImg()
 
       wx.showLoading({
         title: '图片处理中...',
       })
 
-      const { safeCheckResults, fileID } = await this.uploadToCloudAndCheck(imgPaths)
+      const { safeCheckResults, fileID } = await this.uploadToCloudAndCheck(imgPath)
+
       wx.hideLoading()
 
       // 这里先处理正常的，再处理异常
@@ -39,32 +49,21 @@ Page({
         content: '图片校验出错，请重试',
         showCancel: false,
       })
-    } catch (err) {
-      console.log(err)
-      wx.showToast({
-        title: '页面出错',
-        icon: 'none',
-        duration: 2000,
-        success() {
-          setTimeout(
-            wx.reLaunch({
-              url: 'pages/smart-crop/smart-crop',
-            }), 2000)
-        }
-      })
+    } catch (error) {
+      console.log(error)
     }
   },
 
   //选择图片
   async chooseImg() {
 
-    const temImg = await wx.chooseImage({
+    const { tempFilePaths } = await wx.chooseImage({
       count: 1,
       sizeType: ['original', 'compressed'],
       sourceType: ['album', 'camera'],
     })
 
-    let bigPic = temImg.tempFilePaths[0]
+    let bigPic = tempFilePaths[0]
     this.setData({
       bigPic: bigPic
     })
@@ -78,13 +77,14 @@ Page({
     const fileID = await uploadFileToCloud(imgPaths)
     //图片安全校验
     const checkResults = await imgSecCheck(fileID)
-    const safeCheckResults = checkResults.result;
+
+    const safeCheckResults = checkResults.result
     return { safeCheckResults, fileID }
   },
 
   //智能缩小
   async detectImageLabel(fileID) {
-    let that = this
+
     const res = await wx.cloud.callFunction({
       name: 'detect-image-label',
       data: {
@@ -94,27 +94,16 @@ Page({
 
     if (res.result.status === 0) {
       const { list } = res.result.data
-      that.setData({
+      this.setData({
         imgTags: list,
       })
       return
     }
 
     wx.showToast({
-      title: '图片校验出错，请重试',
+      title: '图片校验出错，请重试', // ???
       icon: 'none',
       duration: 2000
     })
-  },
-
-  //分享
-  onShareAppMessage: function () {
-    const DEFAULT_SHARE_COVER = 'https://n1image.hjfile.cn/res7/2020/02/02/a374bb58c4402a90eeb07b1abbb95916.png'
-
-    return {
-      title: '人像魅力',
-      imageUrl: DEFAULT_SHARE_COVER,
-      path: '/pages/smart-crop/smart-crop'
-    }
   },
 })
